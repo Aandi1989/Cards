8d256de7eafe78de3d99ad7b548824fbc39ed0be
Aliaksandr Novik <navaliny1989@gmail.com>
2022-07-05 18:21:58 +0300

    PacksList +RangeSlider

--- src/components/PacksList/PacksList.tsx
+++ src/components/PacksList/PacksList.tsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { RangeSlider } from "../RangeSlider/RangeSlider";
 import classes from './PacksList.module.css';
 
 
 
 export const PacksList=()=>{
 
+    type PacksCategoryType='my'|'all';
+    const[currentPacksCategory,setCurrentPacksCategory]=useState<PacksCategoryType>('all')
+
+    const setCurrentPacksCategoryHandler=(category:PacksCategoryType)=>()=>{
+        setCurrentPacksCategory(category)
+    }
+
     return(
-        <div className={classes.main}>
+        <div className={classes.main}> 
             <div className={classes.container}>
-                <div className={classes.container__navbar}>navbar</div>
-                <div className={classes.container__packsBox}>If you use React Router you might have noticed they recently added a number of useful hooks, specifically useParams, useLocation, useHistory, and use useRouteMatch. But let's see if we can make it even simpler by wrapping them up into a single useRouter hook that exposes just the data and methods we need. In this recipe we show how easy it is to compose multiple hooks and combine their returned state into a single object. It makes a lot of sense for libraries like React Router to offer a selection of low-level hooks, as using only the hook you need can minimize unnecessary re-renders. That said, sometimes you want a simpler developer experience and custom hooks make that easy.
-                If you use React Router you might have noticed they recently added a number of useful hooks, specifically useParams, useLocation, useHistory, and use useRouteMatch. But let's see if we can make it even simpler by wrapping them up into a single useRouter hook that exposes just the data and methods we need. In this recipe we show how easy it is to compose multiple hooks and combine their returned state into a single object. It makes a lot of sense for libraries like React Router to offer a selection of low-level hooks, as using only the hook you need can minimize unnecessary re-renders. That said, sometimes you want a simpler developer experience and custom hooks make that easy.
-                If you use React Router you might have noticed they recently added a number of useful hooks, specifically useParams, useLocation, useHistory, and use useRouteMatch. But let's see if we can make it even simpler by wrapping them up into a single useRouter hook that exposes just the data and methods we need. In this recipe we show how easy it is to compose multiple hooks and combine their returned state into a single object. It makes a lot of sense for libraries like React Router to offer a selection of low-level hooks, as using only the hook you need can minimize unnecessary re-renders. That said, sometimes you want a simpler developer experience and custom hooks make that easy.</div>
+                 <div className={classes.container__navbar}>
+                    <div className={classes.container__navbar__buttonsTitle}>Show packs cards</div>
+                    <div className={classes.container__navbar__buttonsBox}>
+                        <div onClick={setCurrentPacksCategoryHandler('my')} 
+                        className={currentPacksCategory=='my' ? classes.navbar__buttonBox__my_active : classes.navbar__buttonBox__my}>My</div>
+                        <div onClick={setCurrentPacksCategoryHandler('all')} 
+                        className={currentPacksCategory=='all' ? classes.navbar__buttonBox__all_active : classes.navbar__buttonBox__all}>All</div>
+                    </div>
+                    <div className={classes.container__navbar__rangeTitle}>Number of cards</div>
+                    <RangeSlider maxCardsCount={100} minCardsCount={0} onChangeCommitted={function (values: number[]): void {
+                        throw new Error("Function not implemented.");
+                    } }/>
+                 </div>
+                <div className={classes.container__packsBox}></div>
             </div>
-        </div>
+        </div>   
+               
+       
     )
 }
